0158ccf7efbd58bc4fe8fef1b66aeca2f3460af4
jessica joseph <jessicajoseph2019@yahoo.com>
2022-08-05 16:47:41 +0100

    fix: working on the heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('database.json'); // <== Will be created later
+const router = jsonServer.router('./database.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 7000; // <== You can change the port
 server.use(middlewares);
